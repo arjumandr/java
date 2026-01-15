993a6ab7ac9ebb674c11cc5ba8472072e3bb5653
Arjumand Rashid <VE00YN363@YMI.COM>
2026-01-15 12:15:10 +0530

    Trial

---
+++ day1/src/ques1/UML.java
@@ -0,0 +1,14 @@
+package ques1;
+
+class Circle{
+	private int radius;
+	Circle(int radius){
+		this.radius= radius; 
+	}
+}
+public class UML {
+	public static void main(String[] args) {
+		Circle c = new Circle(12);
+		
+	}
+}
